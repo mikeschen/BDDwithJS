d954a6f511d91357c5fdbfa368a915ae6c833026
Michael Schennum and Trevor Elvey <mikeschenphoto@gmail.com>
2016-01-21 11:19:37 -0800

    added reset button

--- Desktop/thursday/js/scripts.js
+++ Desktop/thursday/js/scripts.js
@@ -1,11 +1,25 @@
-
 var maxNumbers = [];
-var count;
 var countUp = function(countUptoNumber, incrementNumber) {
+  var count;
   for (count = incrementNumber; count <= countUptoNumber; count += incrementNumber) {
     maxNumbers.push(count);
   }
   return maxNumbers;
-
 }
-countUp(countUptoNumber, incrementNumber);
+var maxNumbers = [];
+$(document).ready(function() {
+  $("form#Numbers").submit(function(event) {
+    var userCount = parseInt($("input#CountUp").val());
+    var userIncrement = parseInt($("input#IncrementUp").val());
+    countUp(userCount, userIncrement);
+    $("#numberName").empty().append(maxNumbers.join(" , "));
+    $("#result").show();
+
+    event.preventDefault();
+  })
+
+  $("form#Reset").submit(function(event) {
+    $("#numberName")[0].reset();
+    event.preventDefault();
+  })
+});
--- Desktop/thursday/specs/specs.js
+++ Desktop/thursday/specs/specs.js
@@ -1,15 +1,15 @@
 describe('countUp', function() {
 
-  // it("increments by 1", function() {
-  //   expect(countUp(3, 1)).to.eql([1, 2, 3])
-  // });
+  it("increments by 1", function() {
+    expect(countUp(3, 1)).to.eql([1, 2, 3])
+  });
 
   it("increments by 5", function() {
     expect(countUp(15, 5)).to.eql([5, 10, 15])
   });
 
   it("counts up to countUptoNumber", function() {
-    expect(countUp(30, 20)).to.eql([10, 20, 30])
+    expect(countUp(30, 10)).to.eql([10, 20, 30])
   });
 
 });
